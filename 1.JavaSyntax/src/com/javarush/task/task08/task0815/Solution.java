e4353f36bd0693fd450ae8a1e6b55be8dcc6114e
Maksim Kishinskiy <kmaxk2006@mail.ru>
2019-11-05 16:29:32 +0400

    Задача решена 19.11.05

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0815/Solution.java
@@ -0,0 +1,56 @@
+package com.javarush.task.task08.task0815;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/* 
+Перепись населения
+
+Создать словарь (Map<String, String>) занести в него десять записей по принципу «Фамилия» - «Имя».
+Проверить сколько людей имеют совпадающие с заданным именем или фамилией.
+
+Требования:
+1.	Программа не должна выводить текст на экран.
+2.	Программа не должна считывать значения с клавиатуры.
+3.	Метод createMap() должен создавать и возвращать словарь Map с типом элементов String, String состоящих из 10 записей по принципу &laquo;Фамилия&raquo; - &laquo;Имя&raquo;.
+4.	Метод getCountTheSameFirstName() должен возвращать число людей у которых совпадает имя.
+5.	Метод getCountTheSameLastName() должен возвращать число людей у которых совпадает фамилия.
+
+*/
+
+public class Solution {
+    public static Map<String, String> createMap() {
+        Map<String, String> people = new HashMap<>();
+        people.put("Петров", "Василий");
+        people.put("Моисеенко", "Алексей");
+        people.put("Семиколенов", "Виктор");
+        people.put("Родионов", "Михаил");
+        people.put("Михайлова", "Светлана");
+        people.put("Заречный", "Михаил");
+        people.put("Кононова", "Светлана");
+        people.put("Устименко", "Алексей");
+        people.put("Сладкова", "Анна");
+        people.put("Петрухина", "Ольга");
+        return people;
+    }
+
+    public static int getCountTheSameFirstName(Map<String, String> map, String name) {
+        int count = 0;
+        for (Map.Entry<String, String> pair : map.entrySet()) {
+            if (pair.getValue().equals(name)) count++;
+        }
+        return count;
+    }
+
+    public static int getCountTheSameLastName(Map<String, String> map, String lastName) {
+        int count = 0;
+        for (Map.Entry<String, String> pair : map.entrySet()) {
+            if (pair.getKey().equals(lastName)) count++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
